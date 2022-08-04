3984d50edc51c425be31ca0e38009b7e7facfbde
Daniel Alferes <dancon.alferes@gmail.com>
2022-08-04 19:22:55 -0300

    Inicio da estilização sidebar

--- layout_adm/js/custom_adm.js
+++ layout_adm/js/custom_adm.js
@@ -13,3 +13,13 @@ function dropMenu(selector) {
         dropdownMenu.classList.contains("active")? dropdownMenu.classList.remove("active") : dropdownMenu.classList.add("active");
     })
 }
+
+// Sidebar
+let sidebar = document.querySelector(".sidebar"),
+bars = document.querySelector(".bars")
+
+bars.addEventListener("click", () => {
+    sidebar.classList.contains("active") ? sidebar.classList.remove("active") : sidebar.classList.add("active");
+})
+
+window.matchMedia("(max-width: 768px)").matches ? sidebar.classList.remove("active") : sidebar.classList.add("active");
