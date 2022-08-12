34e34f0b17441c232f54b5567baa63e3138962e2
Daniel Alferes <dancon.alferes@gmail.com>
2022-08-12 19:01:47 -0300

    Estilo para tabela - dropdown

--- layout_adm/js/custom_adm.js
+++ layout_adm/js/custom_adm.js
@@ -23,3 +23,27 @@ bars.addEventListener("click", () => {
 })
 
 window.matchMedia("(max-width: 768px)").matches ? sidebar.classList.remove("active") : sidebar.classList.add("active");
+
+// Botão ação do listar
+function actionDropdown(id) {
+    closeDropdownAction();
+    document.getElementById("actionDropdown" + id).classList.toggle("show-dropdown-action")
+}
+
+window.onclick = function(event) {
+    if(!event.target.matches('.dropdown-btn-action')) {
+       // document.getElementById('actionDropdown').classList.remove('show-dropdown-action')
+       closeDropdownAction();
+    }
+}
+
+function closeDropdownAction() {
+    var dropdowns = document.getElementsByClassName('dropdown-action-item')
+    var i;
+    for(i = 0; i < dropdowns.length; i++) {
+         var openDropdown = dropdowns[i]
+         if(openDropdown.classList.contains('show-dropdown-action')) {
+            openDropdown.classList.remove("show-dropdown-action")
+         }
+    }
+}
