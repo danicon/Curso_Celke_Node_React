07390a479d7dadce7eca461b682b2c43f762b5b5
Daniel Alferes <dancon.alferes@gmail.com>
2022-12-02 21:20:38 -0300

    Crud api

--- 10_Aula_Mysql-API/README.md
+++ 10_Aula_Mysql-API/README.md
@@ -4,3 +4,12 @@ Criar o arq package
 ### npm init
 
 ### npm install express
+
+Rodar o projeto
+### node app.js
+
+Acessar
+### http://localhost:8080
+
+Modulo para reiniciar servidor sempre q tiver alguma alteração, g globalmente
+### npm install -g nodemon
--- 10_Aula_Mysql-API/app.js
+++ 10_Aula_Mysql-API/app.js
@@ -2,10 +2,55 @@ const express = require('express')
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+app.get('/usuarios', (req, res) => {
     return res.json({
         erro: false,
         usuario: "Daniel",
         email: "dancon.alferes@gmail.com"
     })
+})
+
+app.get('/usuario/:id', (req, res) => {
+    const {id} = req.params;
+    return res.json({
+        erro: false,
+        id: id,
+        usuario: "Daniel",
+        email: "dancon.alferes@gmail.com"
+    })
+})
+
+app.post('/usuario', (req, res) => {
+    const {nome, email} = req.body;
+    return res.json({
+        erro: false,
+        nome,
+        email
+    })
+})
+
+app.put('/usuario', (req, res) => {
+    const {id, nome, email} = req.body;
+    return res.json({
+        erro: false,
+        id,
+        nome,
+        email
+    })
+})
+
+app.delete('/usuario/:id', (req, res) => {
+    const {id} = req.params;
+    return res.json({
+        erro: false,
+        id
+    })
+})
+
+
+
+app.listen(8080, () => {
+    console.log("Servidor iniciado na porta 8080 http://localhost:8080")
 })
